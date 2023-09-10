bacd4f4f8cec604ed760dab1b946d20c0e5e4742
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-10 15:20:26 +1000

    user to input assignment name

--- Assignment1.java
+++ Assignment1.java
@@ -2,9 +2,15 @@
 import java.util.Scanner;
 
 public class Assignment1{
-    Public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);  
-      //
+    public static void main(String[] args) {
+        // To display the name of the Assignment//
+
+      System.out.println("Enter the name of your Assignment: Assessment01");
+      Scanner scanner = new Scanner(System.in); 
+      String assignmentName = scanner.nextLine();
+      
+      
+      
         
     }
     
