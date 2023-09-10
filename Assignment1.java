797c731df301405f096617360a7ef4c1b039ec26
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-10 15:28:04 +1000

    students marks between 0 to 30 for 30 students

--- Assignment1.java
+++ Assignment1.java
@@ -5,9 +5,13 @@ public class Assignment1{
     public static void main(String[] args) {
         // To display the name of the Assignment//
 
-      System.out.println("Enter the name of your Assignment: Assessment01");
+      System.out.println("Enter the name of your Assignment: Assessment1");
       Scanner scanner = new Scanner(System.in); 
       String assignmentName = scanner.nextLine();
+      
+      // input student's marks for the Assignment1//
+      // lets say we have 30 students,lowest marks can be 0 and highest marks can be 30//
+      
       
       
       
