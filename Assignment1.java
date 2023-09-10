22863ba4e61d9427f254d2450c1e0db00d3cb484
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-10 22:10:41 +1000

    f5

--- Assignment1.java
+++ Assignment1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Assignment1{
 
     public static void main(String[] args) {
-        // To display the name of the Assignment//
+        // F1:To display the name of the Assignment//
 
       System.out.println("Name of your Assignment: ");
       Scanner scanner = new Scanner(System.in); 
       String assignmentName = scanner.nextLine();
       
-      // input student's marks for the Assignment1//
+      // F2:input student's marks for the Assignment1//
       // lets say we have 30 students,lowest marks can be 0 and highest marks can be 30//
       // lets Initialize the lowest value//
       int[] marks = new int[30];
@@ -25,7 +25,7 @@ public class Assignment1{
               System.out.print("Student mark" +(i +1) + ":" );
               mark = scanner.nextInt(); // stores the integer input provided bu the user in the mark variable//
               
-              // for invalid input to show an error messege//
+              // F3:for invalid input to show an error messege//
               
               if (mark < 0 || mark > 30) {
                   System.out.println("Invalid mark.");
@@ -34,27 +34,38 @@ public class Assignment1{
             
             marks[i] = mark; // Students Mark has been stored//
             
-            //to update the marks//
-            
-            if (mark > highestMark) {
-                highestMark = mark;
+
             }
             
-            if (mark < lowestMark) {
-                lowestMark= mark;
-            }
-            }
-            
-            // to print the assignment name //
+            //F4: to print the assignment name //
             
             System.out.println("Assignment Name: " + assignmentName);
             
-            // highest and lowest marks//
+            // F5:highest and lowest marks//
             
-            System.out.println("Highest mark: " + highestMark);
-            System.out.println(" Lowest mark: " + lowestMark);
+            double lowestMark= this.lowestMark[0];
             
-            //to calculate and print the mean and standard deviation//
+            for(double lowestmark:this.lowestMark){
+                if(mark<lowestMark){
+                    lowestMark=mark;
+                }
+            }
+                return lowestMark;
+            
+            
+            double highestMark= this.highestMark[30];
+            
+            for(double highestMark:this.highestMark){
+                if(mark>highestMark){
+                    highestMark=mark;
+                }
+            }
+            
+            return highestMark;
+            
+    
+            
+            //F6:to calculate and print the mean and standard deviation//
             
             double sum = 0;
             for (int mark : marks) {
