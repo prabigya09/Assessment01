fffa790ddb1e7302bf1fb73e3b1267e75fcf1041
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-10 15:11:52 +1000

    Assignment name

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Assignment1{
+    Public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);  
+      //
+        
+    }
+    
+}
+
+
