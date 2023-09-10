4e641fa8bd46a95f7362927d74cb10c573f52c73
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-10 21:17:03 +1000

    .

--- Assignment1.java
+++ Assignment1.java
@@ -6,7 +6,7 @@ public class Assignment1{
     public static void main(String[] args) {
         // To display the name of the Assignment//
 
-      System.out.println("Enter the name of your Assignment: Assessment1");
+      System.out.println("Enter the name of your Assignment: ");
       Scanner scanner = new Scanner(System.in); 
       String assignmentName = scanner.nextLine();
       
